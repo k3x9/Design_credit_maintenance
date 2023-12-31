5e9b201202a77161f8ca881051ec31472544000b
k3x9 <adarsh.shrivastava2003@gmail.com>
2023-12-31 18:29:56 +0530

    name change

---
+++ backend_django/backend_dj/urls.py
@@ -0,0 +1,38 @@
+"""
+URL configuration for backend_django project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from users import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('form_submission/', views.form_submission, name='form_submission'),
+    path('get_forms/', views.get_forms, name='get_forms'),
+    path('approve_form/', views.approve_form, name='approve_form'),
+    path('reject_form/', views.reject_form, name='reject_form'),
+    path('get_forms_super/', views.get_forms_super, name='get_forms_super'),
+    path('check_cookie/', views.check_cookie, name='check_cookie'),
+    path('grade_given/', views.grade_given, name='grade_given'),
+    path('get_forms_by_roll_number/', views.get_forms_by_roll_number, name='get_forms_by_roll_number'),
+    path('get_all/', views.get_all, name='get_all'),
+    path('student_projects/', views.student_projects, name='student_projects'),
+    path('supervisor_projects/', views.supervisor_projects, name='supervisor_projects'),
+    path('get_forms_by_course_code/', views.get_forms_by_course_code, name='get_forms_by_course_code'),
+    path('get_user_department/', views.get_user_department, name='get_user_department'),
+]
