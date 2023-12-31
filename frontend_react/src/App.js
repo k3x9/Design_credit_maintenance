2ff0d44ba90ca293ba7d2cb44756dd9d4b5e8030
k3x9 <adarsh.shrivastava2003@gmail.com>
2023-12-31 17:08:58 +0530

    update

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -3,21 +3,10 @@ import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Signinon from "./routes/sign_in_on";
 import Home from "./routes/home";
-import Layout from "./routes/default";
 import ViewAllForms from "./routes/ViewAllForms";
 import React, { useState } from "react";
-import CustomSnackbar from "./components/snack_bar/toast";
 
 function App() {
-  const [snackbarOpen, setSnackbarOpen] = useState(false);
-
-  const openSnackbar = () => {
-    setSnackbarOpen(true);
-  };
-
-  const closeSnackbar = () => {
-    setSnackbarOpen(false);
-  };
 
   return (
     <BrowserRouter>
@@ -26,9 +15,6 @@ function App() {
         <Route path="/login" element={<Signinon />} />
         <Route path="/home" element={<Home />} />
         <Route path="/view-all-forms" element={<ViewAllForms />} />
-        {/* <Route path="/video/:id" element={<VideoDetail />} />
-        <Route path="/Channel/:id" element={<ChannelDetail />} />
-        <Route path="/Search/:searchTerm" element={<SearchFeed />} /> */}
         
     </Routes>
     </BrowserRouter >
