87dc836ff6a32e3ef2b168d27be90985ac350115
k3x9 <adarsh.shrivastava2003@gmail.com>
2023-12-31 20:02:55 +0530

    update

--- backend_django/build.sh
+++ backend_django/build.sh
@@ -8,6 +8,3 @@ echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 python3.9 manage.py migrate --run-syncdb --noinput
-
-echo "Collect static files..."
-python3.9 manage.py collectstatic --noinput
