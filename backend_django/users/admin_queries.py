c98e0d51c399f2a92025a0bc99bef838450facdc
k3x9 <adarsh.shrivastava2003@gmail.com>
2023-12-31 19:32:00 +0530

    update

--- backend_django/users/admin_queries.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
-from .models import User,Cookie,Student,Supervisor,FacultyAdvisor,Form
-from django.views.decorators.csrf import csrf_protect,ensure_csrf_cookie,csrf_exempt
-from django.middleware.csrf import get_token
-
--- backend_django/users/models.py
+++ backend_django/users/models.py
@@ -1,39 +1,8 @@
 from django.db import models
 from uuid import uuid4
 from datetime import datetime, timedelta
-from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import Group, Permission
 from django.contrib.postgres.fields import ArrayField
-
-# Create your models here.
-# ytb user model
-
-# class User(models.Model):
-#     name = models.CharField(max_length=30)
-#     email = models.CharField(max_length=50)
-#     password = models.CharField(max_length=20)
-#     roll_number = models.CharField(max_length=10)
-#     isFA = models.BooleanField(default=False)
-#     isProf = models.BooleanField(default=False)
-#     isStudent = models.BooleanField(default=False)
-#     department = models.CharField(max_length=30)
-#     design_credits = models.IntegerField(default=0)
-#     category1 = models.IntegerField(default=0)
-#     category2 = models.IntegerField(default=0)
-#     category3 = models.IntegerField(default=0)
-#     category4 = models.IntegerField(default=0)
-#     category5 = models.IntegerField(default=0)
-#     category6 = models.IntegerField(default=0)
-
-# class Form(models.Model):
-#     FormID = models.CharField(max_length=10)
-#     SupervisorID = models.CharField(max_length=20)
-#     roll_number = models.CharField(max_length=10)
-#     course_code = models.CharField(max_length=10)
-#     category = models.IntegerField(default=3)
-#     title = models.CharField(max_length=100)
-#     description = models.CharField(max_length=1000)
-#     outside = models.BooleanField(default=False)
-
 
 class User(models.Model):
     email = models.CharField(max_length=50, primary_key=True)
